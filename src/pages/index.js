0b81cf00ccd4168126a4af911f899fba411b7200
Apocryphon06 <hrithikchandrashekar@gmail.com>
2023-07-02 13:30:36 +0530

    keyFeatures

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
+import { keyPoints, newCeramics } from '@/utils/helper'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,19 +10,19 @@ export default function Home() {
     <main className={` ${inter.className}`}>
       <Header />
 
-      <div className="lg:w-[1290px] lg:m-auto mx-5 mt-20 h-[584px] flex lg:flex-row flex-col-reverse justify-center items-center">
-        <div className="lg:w-[770px] bg-[#2A254B] h-[584px] lg:p-10 p-5 flex flex-col gap-5">
+      <div className="lg:w-[1290px] lg:m-auto mx-5 mt-10 h-[584px] flex lg:flex-row flex-col-reverse justify-center items-center">
+        <div className="lg:w-[770px] bg-[#2A254B] lg:h-[584px] lg:p-10 md:p-10 p-5 flex flex-col gap-5">
           <div className="lg:w-[513px]">
             <h1 className="text-white lg:text-4xl md:text-3xl text-2xl">
               {' '}
               The furniture brand for the future, with timeless designs
             </h1>
-            <button className="bg-[#726E8D] px-[32px] py-2 text-[#2A254B] mt-5">
+            <button className="bg-[#726E8D] px-[32px] py-4 text-[#2A254B] mt-5">
               View collection
             </button>
           </div>
 
-          <div className=''>
+          <div className="">
             <h2 className="lg:text-lg text-base text-white leading-6 font-light lg:w-[650px] ">
               A new era in eco friendly furniture with Avelon, the French luxury
               retail brand with nice fonts, tasteful colors and a beautiful way
@@ -30,7 +31,55 @@ export default function Home() {
           </div>
         </div>
 
-        <img src="/assets/images/dandyChair.jpg" />
+        <img
+          src="/assets/images/dandyChair.jpg"
+          alt=""
+          className="lg:block hidden"
+        />
+      </div>
+
+      <div className="flex flex-col justify-center items-center lg:mt-20 mt-10 lg:m-auto mx-5">
+        <h2 className="lg:text-2xl text-xl text-[#2A254B]">
+          What makes our brand different
+        </h2>
+
+        <div className="lg:w-[1290px] lg:m-auto mx-5">
+          <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-10 mt-10">
+            {keyPoints.map((item, index) => (
+              <div
+                key={index + 1 + item.name}
+                className="flex flex-col justify-start gap-2"
+              >
+                <Image src={item.icon} alt={item.icon} width={24} height={24} />
+                <h3 className="lg:text-xl text-lg text-[#2A254B]">
+                  {item.title}
+                </h3>
+                <p className="text-base">{item.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <br/>
+      <br/>
+
+      <div className="lg:w-[1290px] lg:m-auto mx-5 flex flex-col justify-center items-center">
+        <h2 className="lg:text-4xl md:text-3xl text-2xl text-[#2A254B]">
+          New Ceramics
+        </h2>
+
+        <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-10 justify-center items-center mt-10">
+          {newCeramics.map((item, index) => (
+            <div
+              key={index + 1 + item.name}
+              className="flex flex-col justify-start items-start gap-2"
+            >
+              <Image src={item.image} alt={item.name} width={292} height={359} className="object-fit w-[292px] h-[359px]"/>
+              <h3 className='text-lg'>{item.name}</h3>
+              <p className='text-base'>{item.price}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   )
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,31 +1,79 @@
+
 export const menuItems = [
+  {
+    name: 'Plant pots',
+    link: '#',
+  },
+  {
+    name: 'Ceramics',
+    link: '#',
+  },
+  {
+    name: 'Tables',
+    link: '#',
+  },
+  {
+    name: 'Chairs',
+    link: '#',
+  },
+  {
+    name: 'Crockery',
+    link: '#',
+  },
+  {
+    name: 'Tableware',
+    link: '#',
+  },
+  {
+    name: 'Cutlery',
+    link: '#',
+  },
+]
+
+export const keyPoints = [
+  {
+    title: 'Next day as standard',
+    description: 'Order before 3pm and get your order the next day as standard',
+    icon: '/assets/icons/delivery.svg',
+  },
+  {
+    title: 'Made by true artisans',
+    description: 'Handmade crafted goods made with real passion and craftmanship',
+    icon: '/assets/icons/checkmark.svg',
+  },
+  {
+    title: 'Unbeatable prices',
+    description: 'For our materials and quality you won’t find better prices anywhere',
+    icon: '/assets/icons/credit.svg',
+  },
+  {
+    title: 'Recycled packaging',
+    description: 'We use 100% recycled packaging to ensure our footprint is manageable',
+    icon: '/assets/icons/sprout.svg',
+  },
+]
+
+
+export const newCeramics = [
     {
-        name:'Plant pots',
-        link:'#'
+        name:'The Dandy Chair',
+        price:'$250',
+        image:'/assets/images/dandyChair.jpg'
     },
     {
-        name:'Ceramics',
-        link:'#'
+        name:'Rustic Vase Set',
+        price:'$250',
+        image:'/assets/images/vase.png'
     },
     {
-        name:'Tables',
-        link:'#'
+        name:'The Silky Vase',
+        price:'$250',
+        image:'/assets/images/silkVase.png'
     },
     {
-        name:'Chairs',
-        link:'#'
+        name:'The Lucy Lamp',
+        price:'$250',
+        image:'/assets/images/lamp.png'
     },
-    {
-        name:'Crockery',
-        link:'#'
-    },
-    {
-        name:'Tableware',
-        link:'#'
-    },
-    {
-        name:'Cutlery',
-        link:'#'
-    },
-    
+
 ]
